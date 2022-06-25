b0a208bfc15d0213455b252aec0801b63b37da5b
alkhioz <33111241+Alkhioz@users.noreply.github.com>
2022-06-25 15:00:07 -0500

    Removed third digit verification

--- client/src/utilities/validarCedula.js
+++ client/src/utilities/validarCedula.js
@@ -1,3 +1,5 @@
+//https://medium.com/@bryansuarez/c%C3%B3mo-validar-c%C3%A9dula-y-ruc-en-ecuador-b62c5666186f#:~:text=El%20proceso%20para%20la%20validaci%C3%B3n,3%2C4%2C5).
+//https://www.jybaro.com/blog/cedula-de-identidad-ecuatoriana/
 const validarCedula = (cedula) => {
     //Check DNI length
     if(cedula.length!=10)
@@ -13,12 +15,12 @@ const validarCedula = (cedula) => {
             msg: "La cédula no pertenece a una región valida"
         };
     //Check third digit
-    let thirdDigit = cedula[2];
+    /*let thirdDigit = cedula[2];
     if(thirdDigit < 0 || thirdDigit >5)
         return {
             status: false,
             msg: "Tercer digito no es valido"
-        };
+        };*/
     //Taking the first 9 digits
     let odds = [cedula[0], cedula[2], cedula[4], cedula[6], cedula[8]].map(odd => odd*2>9?odd*2-9:odd*2);
     let evens = [cedula[1], cedula[3], cedula[5], cedula[7]].map(even => parseInt(even));
