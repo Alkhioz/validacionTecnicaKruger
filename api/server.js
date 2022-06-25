856d9e4a68beab3ecfecfa5829cbb07ef19c70bc
alkhioz <33111241+Alkhioz@users.noreply.github.com>
2022-06-24 23:53:56 -0500

    Added logic to control user insert, delete, update and get

--- api/server.js
+++ api/server.js
@@ -12,6 +12,37 @@ const secret = "thisIsTheSecretCode224411222.////####"
 
 const createToken = (data) => jwt.sign({data}, secret, { expiresIn: '1h' })
 
+const validateToken = (token) => jwt.verify(token, secret, (err, decoded)=>{
+    if(err)
+        return false
+    return decoded
+})
+
+const auth = (req, res, next) => {
+    let decoded = validateToken(req.headers["access-token"])
+    if (decoded) {
+        req.decoded = decoded.data
+        next()
+    } else {
+        res.jsonp({
+            msg: 'err',
+            data: {description: 'invalid token'}
+        });
+    }
+}
+
+const isAdmin = (req, res, next) => {
+    let user = db.roleByUser.find(role=>{
+        return role.userId == req.decoded
+    })
+    if (user.roleId == 1)
+        next()
+    res.jsonp({
+            msg: 'err',
+            data: {description: "user cant access this info"}
+        })
+}
+
 server.post('/login', (req, res) => {
     let {username, password} = req.body
     let user = db.user.find(user=>{
@@ -32,9 +63,10 @@ server.post('/login', (req, res) => {
     
 })
 
-server.get('/user/:id', (req, res) => {
+server.get('/getCurrentUserData', auth, (req, res) => {
+
     let user = db.user.find(user => {
-        return user.id == req.params.id
+        return user.id == req.decoded
     })
     let role = db.roleByUser.find(role =>{
         return role.userId == user.id
@@ -47,7 +79,32 @@ server.get('/user/:id', (req, res) => {
     res.jsonp(response)
 })
 
+server.get('/getUsers', auth, isAdmin, (req, res) => {
+    let users = db.user.map(user =>{
+        let data = {
+            name: user.name,
+            lastname: user.lastname,
+            dni: user.dni,
+            mail: user.email,
+            username: user.username,
+            address: user.address,
+            phone: user.phone,
+            birth: user.dateOfBirth
+        }
+        return data
+    })
+    res.jsonp({
+        msg: 'ok',
+        data: {users}
+    });
+})
+
+server.post('addUser', (req, res) => {
+    
+})
+
 server.use(middlewares)
+server.use(auth)
 server.use(router)
 server.listen(8080, () => {
   console.log('JSON Server is running')
