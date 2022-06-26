35af0b5effa3d5c557f1289e80d31e92cc27b99a
alkhioz <33111241+Alkhioz@users.noreply.github.com>
2022-06-26 09:54:35 -0500

    Added initial data to inicio layout

--- client/src/layouts/inicio.js
+++ client/src/layouts/inicio.js
@@ -1,12 +1,28 @@
 import './inicio.css';
-//import IconButton from '../components/iconbutton/IconButton.js';
+import IconButton from '../components/iconbutton/IconButton.js';
+import { useNavigate  } from "react-router-dom";
 
 function Inicio(){
-
+    const navigate = useNavigate();
     return(
         <div className="inicioLayout">
-                <h1>Inventario de vacunación Kruger</h1>
-                <h2>Estado de vacunación a nivel empresarial</h2>
+            <div className="inicioCard">
+                <header className="inicioHeader">
+                    <h1 className="inicioTitulo">IVK Kruger Corp</h1>
+                    <h2 className="inicioSubtitulo">Inventario de vacunación de empleados</h2>
+                </header>
+                <main className="inicioMain">
+                    <p className="inicioMainText">En su compromiso con mantener la salud de su personal y del público en general, Kurger Corp pone a disposición de sus empleados esta herramienta donde pueden actualizar el estado de su vacunación.</p>                    <div className="inicioAction">
+                        <IconButton 
+                            id="adddata"
+                            onClick={()=>navigate("/login")}
+                            name="Actualizar estado de vacunación"
+                            icon="fa fa-paper-plane"
+                            type="IconButtonAlter"
+                        />  
+                    </div>
+                </main>
+            </div>        
         </div>
     );
 }
