afa5f1fde0fdc2a86b9d2a73dacae84aa458a66d
alkhioz <33111241+Alkhioz@users.noreply.github.com>
2022-06-26 11:40:04 -0500

    Adding initial sidebar style for mobile users

--- client/src/layouts/main.js
+++ client/src/layouts/main.js
@@ -4,16 +4,17 @@ import { useNavigate  } from "react-router-dom";
 import useUser from '../data/use-user.js';
 import {useEffect, useState} from 'react';
 import Loading from './loading.js';
+import IconButton from '../components/iconbutton/IconButton.js';
 
 function Inicio(){
 
     const { user, mutate, loggedOut, loading } = useUser();
     const [navState, setNavState] = useState(0);
+    const [showMobileNavState, setShowMobileNavState] = useState(false);
     
     const handleChangeNav = (evt) => {
         evt.preventDefault();
         setNavState(evt.target.value);
-        console.log(evt.target.value);
     }
 
     const navigate = useNavigate();
@@ -28,30 +29,57 @@ function Inicio(){
         logout();
         mutate("/getCurrentUserData");
     }
+
+    const handleShowMobileMenu=(evt)=> {
+        evt.preventDefault();
+        setShowMobileNavState(!showMobileNavState);
+    }
+
+    const makeMobileMenu=()=>{
+        return(
+            <div className="mainMobileMenu" style={{display: showMobileNavState?"block":"none"}}>
+                <div className="mainMobileMenuCardHeader">
+                        <button className="mainMobileMenuCardHeaderButton" onClick={handleShowMobileMenu}><i className="fa fa-times-circle" aria-hidden="true"></i></button>
+                </div>
+                <div className="mainMobileMenuCard">
+                    <p><b>Bienvenido </b>{user.name}</p>
+                    {(!loggedOut&&user.isAdmin)&&<p>admin</p>}
+                    <IconButton 
+                        id="logout_mobile"
+                        name="Salir"
+                        icon="fa fa-sign-out"
+                        type="IconButtonAlter"
+                        onClick={handleLogout}
+                    />
+                </div>
+            </div>
+        );
+    }
+    
     const makeAdminMenu=()=>{
         return(
             <div className="mainAdminMenu">
-                <button className="mainAdminMenuItem" onClick={handleChangeNav} value="0">Dashboard<i className="fa fa-pie-chart" aria-hidden="true"></i></button>
-                <button className="mainAdminMenuItem" onClick={handleChangeNav} value="1">Usuarios<i className="fa fa-users" aria-hidden="true"></i></button>
+                <button className={parseInt(navState)===0?"mainAdminMenuItem mainAdminMenuItemSelected":"mainAdminMenuItem"} onClick={handleChangeNav} value="0">Dashboard<i className="fa fa-pie-chart" aria-hidden="true"></i></button>
+                <button className={parseInt(navState)===1?"mainAdminMenuItem mainAdminMenuItemSelected":"mainAdminMenuItem"} onClick={handleChangeNav} value="1">Usuarios<i className="fa fa-users" aria-hidden="true"></i></button>
             </div>
         );
     }
     return(
         loading?<Loading/>:
         <div className="mainLayout">
+                {makeMobileMenu()}
                 <nav className="mainNav">
                     <h1 className="mainLogo noSelect">IVK</h1>
                     <div className="mainControl">
                         {(!loggedOut&&user.isAdmin)&&makeAdminMenu()}
                     </div>
                     <div className="mainUser">
-                        <p><b>Welcome </b>{user.name} <a className="mainLogout" href="/login" onClick={handleLogout}>logout</a></p>
+                        <p><b>Bienvenido </b>{user.name} <a className="mainLogout" href="/login" onClick={handleLogout}>Salir</a></p>
                     </div>
                     <div className="mainResponsive">
-                        <button className="mainBarButton"><i className="fa fa-bars"></i></button>
+                        <button onClick={handleShowMobileMenu} className="mainBarButton"><i className="fa fa-bars"></i></button>
                     </div>
                 </nav>
-                
         </div>
     );
 }
