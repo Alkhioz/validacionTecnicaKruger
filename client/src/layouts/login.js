676e2c133915ed785f9c30aeb29cb84211a16e2a
alkhioz <33111241+Alkhioz@users.noreply.github.com>
2022-06-26 09:58:20 -0500

    Changed login logic to make use of form onsubmit

--- client/src/layouts/login.js
+++ client/src/layouts/login.js
@@ -31,7 +31,8 @@ function Login(){
         setStateClave(evt.target.value);
     }
     
-    const handleLogin = async() => {
+    const handleLogin = async(evt) => {
+        evt.preventDefault();
         let authdata = await login(stateUsuario, stateClave);
         if(authdata.msg === "ok"){
             localStorage.setItem("token", authdata.data.token);
@@ -46,28 +47,30 @@ function Login(){
         <div className="loginLayout">
             <div className="loginCard">
                 <h1 className="loginTitle">Login IVK</h1>
-                <Input 
-                    type="text"
-                    name="Usuario"
-                    id="usuario"
-                    value={stateUsuario}invalid user or password
-                    onChange={onChangeUsuario}
-                />
-                <Input 
-                    type="password"
-                    name="Contraseña"
-                    id="contraseña"
-                    value={stateClave}
-                    onChange={onChangeClave}
-                />
-                <IconButton 
-                    id="login"
-                    onClick={handleLogin}
-                    name="Iniciar sesión"
-                    icon="fa fa-sign-in"
-                    type="IconButton"
-                />
-                {stateError!==""&&<p className="LoginError">{stateError}</p>}
+                <form onSubmit={handleLogin}>
+                    <Input 
+                        type="text"
+                        name="Usuario"
+                        id="usuario"
+                        value={stateUsuario}invalid user or password
+                        onChange={onChangeUsuario}
+                    />
+                    <Input 
+                        type="password"
+                        name="Contraseña"
+                        id="contraseña"
+                        value={stateClave}
+                        onChange={onChangeClave}
+                    />
+                    <IconButton 
+                        id="login"
+                       
+                        name="Iniciar sesión"
+                        icon="fa fa-sign-in"
+                        type="IconButton"
+                    />
+                    {stateError!==""&&<p className="LoginError">{stateError}</p>}
+                </form>
             </div>
         </div>
     );
