da7f255482b1242c11937cf52b09d86b41754cbb
alkhioz <33111241+Alkhioz@users.noreply.github.com>
2022-06-26 18:45:24 -0500

    Getting user list and showing name

--- client/src/components/users/users.js
+++ client/src/components/users/users.js
@@ -1,8 +1,24 @@
 import './users.css';
+import UseUsers from '../../data/use-users';
+import { useEffect, useState } from 'react';
 
 const Users=()=>{
+    const { users, noDataUsers, loadingUsers, mutateUsers } = UseUsers();
+
+    const[usersState, setUsersState] = useState([]);
+
+    const userList=()=>usersState.map((user,i)=>{
+        return <p key={`userlist${i}`}>{user.name}</p>
+    });
+
+    useEffect(() => {
+        if (users && !noDataUsers) {
+            setUsersState(users);
+        }
+    }, [users, noDataUsers]);
+
     return(
-        <p>Users</p>
+        !loadingUsers?userList():<></>
     );
 }
 
--- client/src/data/use-users.js
+++ client/src/data/use-users.js
@@ -5,12 +5,12 @@ function useUsers() {
   const fetcher = async (url) => await clienteAxios.get(url).then(res => res.data);
 
   const { data, mutate, error } = useSWR(`/user`, fetcher);
-  const loading = !data && !error;
+  const loadingUsers = !data && !error;
   const noData = error && error.response.status === 403;
   
   return {
-    loadingUsers:loading,
-    noDataUser:noData,
+    loadingUsers,
+    noDataUsers:noData,
     users: data,
     mutateUsers: mutate
   };
