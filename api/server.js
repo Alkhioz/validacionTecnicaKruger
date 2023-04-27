70e3945fc010b593ddff1baf81739e83e5c219b2
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2023-04-26 23:03:32 -0500

    resstore

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('/tmp/db.json');
-const db = require('/tmp/db.json');
+const router = jsonServer.router('db.json');
+const db = require('./db.json');
 const middlewares = jsonServer.defaults()
 const cors = require('cors');
 
